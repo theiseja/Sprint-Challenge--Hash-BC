85ea984c0585e4909ba8f752dad925c659fbff90
Jesse Theisen <theiseja@gmail.com>
2020-03-21 13:22:24 -0500

    Starting work on miner.py

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -19,8 +19,8 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    for i in range(length):
-        hash_table_insert(hashtable,  tickets[i].source, tickets[i].destination)
+    for ticket in tickets:
+        hash_table_insert(hashtable,  ticket.source, ticket.destination)
 
         current_val = hash_table_retrieve(hashtable, "NONE")
         print(current_val)
